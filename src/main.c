96a0e177ac921148b174710e456f6ead3b699875
maziac <thomas.busse@gmx.net>
2025-02-08 19:32:39 +0100

    Error with main address

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
+#include <arch/spectrum.h>
 #include <stdio.h>
 #include <input.h>
 #include "clear-ula.h"
 #include "fibonacci.h"
 #include "factorial.h"
+
 
 
 int global_var = 5;
@@ -12,7 +14,10 @@ int main(void) {
 
     int number;
     while (1) {
-        printf("Please enter an integer number (or -1 to exit): ");
+        *zx_cxy2aaddr(10, 5) = PAPER_GREEN | INK_RED;
+        printf("\x0C");
+        printf("%c%c", 16, 50); // INK Red (50 = '2' + 48)
+        printf("Enter a number (or -1 to exit): ");
         scanf("%d", &number);
 
         if (number == -1) {
@@ -21,34 +26,8 @@ int main(void) {
 
         printf("Factorial(%d) = %d\n", number, factorial(number));
         printf("Fibonacci(%d) = %d\n", number, fibonacci(number));
+        printf("\x0C");
     }
 
     return 0;
-
-    global_var = 10;
-    clear_ula();
-    printf("Fibonacci(10) = %d\n", fibonacci(10));
-    //printf("Factorial(10) = %d\n", factorial(10));
-
-    my_other_var = factorial(5);
-
-    printf("Factorial(5) = %d\n", my_other_var);
-
-    printf("Please type in a number:\n");
-    int input_number;
-    scanf("%d", &input_number);  // Read an integer from the user
-    printf("You entered: %d\n", input_number);
-
-    printf("Please type in some characters:\n");
-    while (1) {
-        int c;
-        // Wait on key press
-        while ((c = in_inkey()) == 0);
-        // Print key
-        printf("%c", c);
-        // Wait on key release
-        while (in_inkey());
-    }
-
-//    return 0;
 }
